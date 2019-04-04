5054e7ffd7be8dc243819839f957da54014a3d09
Merve <47145223+mervegulozcan@users.noreply.github.com>
2019-04-04 17:25:35 +0300

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace HayvanYarisi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnBasla = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBasla
+            // 
+            this.btnBasla.Location = new System.Drawing.Point(15, 23);
+            this.btnBasla.Name = "btnBasla";
+            this.btnBasla.Size = new System.Drawing.Size(88, 23);
+            this.btnBasla.TabIndex = 2;
+            this.btnBasla.Text = "Başla";
+            this.btnBasla.UseVisualStyleBackColor = true;
+            this.btnBasla.Click += new System.EventHandler(this.btnBasla_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnBasla);
+            this.panel1.Location = new System.Drawing.Point(3, 385);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(726, 53);
+            this.panel1.TabIndex = 4;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::HayvanYarisi.Properties.Resources.snake2;
+            this.pictureBox2.Location = new System.Drawing.Point(3, 157);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::HayvanYarisi.Properties.Resources.snake1;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 54);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 46F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(671, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 355);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "B\r\nİ\r\nT\r\nİ\r\nŞ\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(753, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Yılan Yarışı";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnBasla;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HayvanYarisi
+{
+    public partial class Form1 : Form
+    {
+        
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random rnd = new Random();
+        private void btnBasla_Click(object sender, EventArgs e)
+        {
+            timer1.Interval = 100;
+            pictureBox1.Left = 3;
+            pictureBox2.Left = 3;
+            timer1.Start();
+            timer1_Tick(sender,e);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            pictureBox1.Left = pictureBox1.Left + rnd.Next(1, 15);
+            pictureBox2.Left = pictureBox2.Left + rnd.Next(1, 15);
+            if (pictureBox1.Right >= label1.Left || pictureBox2.Right >= label1.Left)
+            {
+                timer1.Stop();
+                string kazanan;
+                if(pictureBox1.Right>=pictureBox2.Right)
+                {
+                    kazanan = "Sarı Yılan";
+                }
+                else
+                {
+                    kazanan = "Yeşil Yılan";
+                }
+                MessageBox.Show($"{kazanan} kazandı.");
+
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
